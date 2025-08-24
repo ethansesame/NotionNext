302f7ab27e113cdc7bbec028438d90f2397cf4fa
Sandy Sesame <sesamerin@outlook.com>
2025-08-24 15:49:09 +0800

    Update SocialButton.js

--- themes/fukasawa/components/SocialButton.js
+++ themes/fukasawa/components/SocialButton.js
@@ -2,40 +2,13 @@ import QrCode from '@/components/QrCode'
 import { siteConfig } from '@/lib/config'
 import { useRef, useState } from 'react'
 import { handleEmailClick } from '@/lib/plugins/mailEncrypt'
-
 /**
  * 社交联系方式按钮组
  * @returns {JSX.Element}
  * @constructor
  */
 const SocialButton = () => {
-  const CONTACT_GITHUB = siteConfig('CONTACT_GITHUB')
-  const CONTACT_TWITTER = siteConfig('CONTACT_TWITTER')
-  const CONTACT_TELEGRAM = siteConfig('CONTACT_TELEGRAM')
-
-  const CONTACT_LINKEDIN = siteConfig('CONTACT_LINKEDIN')
-  const CONTACT_WEIBO = siteConfig('CONTACT_WEIBO')
-  const CONTACT_INSTAGRAM = siteConfig('CONTACT_INSTAGRAM')
-  const CONTACT_EMAIL = siteConfig('CONTACT_EMAIL')
-  const ENABLE_RSS = siteConfig('ENABLE_RSS')
-  const CONTACT_BILIBILI = siteConfig('CONTACT_BILIBILI')
-  const CONTACT_YOUTUBE = siteConfig('CONTACT_YOUTUBE')
-
-  const CONTACT_XIAOHONGSHU = siteConfig('CONTACT_XIAOHONGSHU')
-  const CONTACT_ZHISHIXINGQIU = siteConfig('CONTACT_ZHISHIXINGQIU')
-  const CONTACT_WEHCHAT_PUBLIC = siteConfig('CONTACT_WEHCHAT_PUBLIC')
-  const [qrCodeShow, setQrCodeShow] = useState(false)
-
-  const openPopover = () => {
-    setQrCodeShow(true)
-  }
-  const closePopover = () => {
-    setQrCodeShow(false)
-  }
-
-  const emailIcon = useRef(null)
-
-
-  return ()
+  return null
 }
+
 export default SocialButton
